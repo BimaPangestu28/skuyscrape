b401c7445700711d77a584b8dbe6013c96ea9ee2
Bima Pangestu <bimapangestu280@gmail.com>
2021-01-22 10:46:14 +0700

    Update scrapping.bat

--- scrapping.bat
+++ scrapping.bat
@@ -1,3 +1,3 @@
-"C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python38-32\python.exe" -m pip install -r requirements.txt
-"C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python38-32\python.exe" "C:\Users\%USERNAME%\Documents\skuyscrape\index.py"
+"C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python38\python.exe" -m pip install -r requirements.txt
+"C:\Users\%USERNAME%\AppData\Local\Programs\Python\Python38\python.exe" "C:\Users\%USERNAME%\Documents\skuyscrape\index.py"
 pause
